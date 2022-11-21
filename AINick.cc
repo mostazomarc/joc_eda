35377889213daa6d0bccea56496b103a4688aea2
Marc Mostazo <marc.mostazo@estudiantat.upc.edu>
2022-11-21 11:19:48 +0100

    bfs per buscar un espai per quan estic rodejat de territori meu i vull conquerir(puntuació=

--- AINick.cc
+++ AINick.cc
@@ -53,7 +53,7 @@ struct PLAYER_NAME : public Player
   }
 
   // Returns Dir to the nearest avialable adjacent space or somewhere to move otherwise
-  Dir space_adj(Pos p)
+  Dir space_adj(int id, Pos p)
   {
     Pos act = p;
     if (conq(act + Down))
@@ -67,6 +67,9 @@ struct PLAYER_NAME : public Player
 
     cerr << "no adjacent spaces at pos" << p.i << ',' << p.j << endl;
 
+    return bfs_space(id, act);
+    
+    /*
     if (pos_ok(act + Up) and cell(act + Up).type == Street)
       return Up;
     if (pos_ok(act + Down) and cell(act + Down).type == Street)
@@ -76,6 +79,7 @@ struct PLAYER_NAME : public Player
     if (pos_ok(act + Left) and cell(act + Left).type == Street)
       return Left;
     return Right;
+    */
   }
 
   // retorna si el proxim pas del bfs es una posició accesible i no visitada
@@ -279,6 +283,98 @@ struct PLAYER_NAME : public Player
     }
   }
 
+  // Returns Dir to the nearest avialable food
+  Dir bfs_space(int id, Pos p)
+  {
+    int i = p.i;
+    int j = p.j;
+    int n = 60;
+    int m = 60;
+
+    Pos posnull(-1, -1);
+
+    queue<Pos> Q;                                           // Posicions per mirar (no s'afegirà una pos a la cua si no es accesible)
+    vector<vector<int>> dist(n, vector<int>(m, -1));        // distancia per cada posició
+    vector<vector<Pos>> previs(n, vector<Pos>(m, posnull)); // previ de cada posició visitada (s'haurà de borrar)
+    stack<Pos> camí;
+
+    Q.push(p);          // apuntem primera posició per mirar
+    dist[p.i][p.j] = 0; // distancia primera posició es 0
+
+    bool space = false;
+    Pos posspace = p;
+    while (not Q.empty() and not space) // and dist[act.i][act.j] <= 7) //mentre n'hi hagin posicions per mirar
+    {
+      Pos act = Q.front(); // agafem primera posició i la treiem de la cua
+      Q.pop();
+      camí.push(act); // guardem la posició com camí fet
+
+      // cerr << "pos act a mirar " << act.i << ',' << act.j << endl;
+
+      if (conq(act))
+      {
+        space = true; // si es menjar food = true perque hem trobat menjar
+        posspace = act;
+        cerr << id << " conquerable space at " << act.i << ',' << act.j << " a distancia " << dist[act.i][act.j] << endl;
+      }
+      else
+      {
+        if (proximpas(act, Down, dist))
+        {
+          Q.push(act + Down);                              // afegir pos a la cua
+          dist[act.i + 1][act.j] = dist[act.i][act.j] + 1; // actualitzar distancia
+          previs[act.i + 1][act.j] = act;
+        }
+        if (proximpas(act, Up, dist))
+        {
+          Q.push(act + Up);
+          dist[act.i - 1][act.j] = dist[act.i][act.j] + 1;
+          previs[act.i - 1][act.j] = act;
+        }
+        if (proximpas(act, Left, dist))
+        {
+          Q.push(act + Left);
+          dist[act.i][act.j - 1] = dist[act.i][act.j] + 1;
+          previs[act.i][act.j - 1] = act;
+        }
+        if (proximpas(act, Right, dist))
+        {
+          Q.push(act + Right);
+          dist[act.i][act.j + 1] = dist[act.i][act.j] + 1;
+          previs[act.i][act.j + 1] = act;
+        }
+      }
+    }
+
+    // Si no hem trobat space retornem una dirreció imposible 'DR'
+    if (not space)
+    {
+      cerr << id << " SPACE not found" << endl;
+      return DR;
+    }
+
+    // Obtenim primera posició del camí fet
+    Pos act = posspace;
+    cerr << id << " CONQUERING SPACE at " << act.i << ',' << act.j << "from " << p.i << ',' << p.j << endl;
+    while (previs[act.i][act.j] != p)
+    {
+      act = previs[act.i][act.j];
+    }
+    cerr << id << " anire de p:" << p.i << ',' << p.j << " a nou: " << act.i << ' ' << act.j << endl;
+
+    if (act.i > p.i and accesible(act + Down))
+      return Down;
+    if (act.i < p.i and accesible(act + Up))
+      return Up;
+    if (act.j > p.j and accesible(act + Right))
+      return Right;
+    if (act.j < p.j and accesible(act + Left))
+      return Left;
+
+    return DR;
+  }
+
+
   /**
    * Play method, invoked once per each round.
    */
@@ -310,7 +406,7 @@ struct PLAYER_NAME : public Player
       else
       {
         cerr << "conquistant..." << endl;
-        dir = space_adj(unitpos);
+        dir = space_adj(id,unitpos);
         cerr << "unit " << id << " conquistara cap a " << dir << endl;
         move(alive[id], dir);
       }
